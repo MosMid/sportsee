a66baa1359b8d09f38f7c6a49b2c560b3afbff5a
MosMid <105169046+MosMid@users.noreply.github.com>
2023-02-10 11:16:13 +0100

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import useFetch from '../hooks/useFetch'
 import './Home.css'
 import calories from '../img/calories-icon.png'
@@ -10,23 +10,40 @@ import AverageSessionChart from '../components/AverageSessionChart';
 import PerformanceChart from '../components/PerformanceChart';
 import Score from '../components/Score'
 
-export default function App(){
-    let userUrl = "http://localhost:3000/user/18";
-    const [data,loading]=useFetch(userUrl, null);
+/**
+ * Home component that displays the user's activity data
+ * 
+ * @param {object} props - The component's properties
+ * @param {object} props.mockData - Optional mock data for testing purposes
+ * @param {string} props.backendUser - The user's id used to fetch data from the backend
+ * @returns {JSX.Element} - The component's JSX element
+ */
+export default function App(props){
+    const user = props.mockData
+    const url = "http://localhost:3000/user/"+props.backendUser
+    // destructuring the hook's returned values
+    let [data,loading]=useFetch(url, null);
+    if(props.mockData){
+        data = user.main.data
+    }
+    // handling the loading state
     if(loading){
         return <div>Chargement...</div>
     }
+    // handling the error cases
+    if(data == "can not get user") return <div className='error'>Cannot find user</div>
+    if(data == "server error") return <div className='error'>Server error</div>
     if(data){
         return <div className='Accueil'>
             <h1>Bonjour <span className='firstName'>{data.userInfos.firstName}</span></h1>
             <p className='congrats'>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
             <div className='flex'>
                 <div className='charts'>
-                    <ActivityChart url={userUrl}/>
+                    <ActivityChart data={props.mockData? user.activity.data : null} url={url}/>
                     <div className='bottomCharts'>
-                        <AverageSessionChart url={userUrl}/>
-                        <PerformanceChart url={userUrl}/>
-                        <Score url={userUrl}/>
+                        <AverageSessionChart data={props.mockData? user.averageSessions.data : null} url={url}/>
+                        <PerformanceChart data={props.mockData? user.performance.data : null} url={url}/>
+                        <Score data={props.mockData? user.main.data : null} url={url}/>
                     </div>
                 </div>
                 <div className='status'>
